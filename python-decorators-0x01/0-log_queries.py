ace9da2aab076251eabad3b6e61e6b8390f84f46
ankundah <cynthiaankunda@yahoo.com>
2025-07-12 18:18:04 +0300

    log_queries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,18 +1,19 @@
 import sqlite3
 import functools
 
+#### decorator to lof SQL queries
+
+#  """ YOUR CODE GOES HERE"""
+
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # Extract the query from kwargs or args
         query = kwargs.get('query', None)
         if query is None and len(args) > 0:
             query = args[0] if isinstance(args[0], str) else None
-        
-        # Log the query
+
         print(f"Executing SQL query: {query}")
         
-        # Call the original function
         return func(*args, **kwargs)
     return wrapper
 
