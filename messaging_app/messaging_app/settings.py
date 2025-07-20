289a5fee34fa0d224ca2ba3eba6a75a4f8cd5a6c
ankundah <cynthiaankunda@yahoo.com>
2025-07-20 12:46:29 +0300

    modify settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rest_framework.authentication.SessionAuthentication',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
